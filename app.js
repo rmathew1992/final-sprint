fa53c5a4ff9a24c87823a7b55c7d8e61687530ed
Alec <alec.radford@gmail.com>
2013-04-22 21:17:42 -0400

    Facebook integration?

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
+  , Facebook = require('facebook-node-sdk')
   , mongoose = require('mongoose');
 
 var app = express();
@@ -20,16 +21,41 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser(process.env.COOKIE_SECRET));
+  app.use(express.session({secret: ' hi wolfie =) '}));
+  app.use(Facebook.middleware({ appId: '592198594123789', secret: '5c39850ad7130c483f719d42ec19252f' }));
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
+  app.set('host', 'localhost:' + process.env.PORT)
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//req.user is actually facebook ID not name
+function facebookGetUser() {
+  return function(req, res, next) {
+    req.facebook.getUser( function(err, user) {
+      if (!user || err){
+        res.render('login', {title: "How's life?"})
+      } else {
+        req.session.fbid = user;
+        next();
+      }
+    });
+  }
+}
+
+app.get('/', facebookGetUser(), index.home);
+app.get('/login', Facebook.loginRequired(), function(req, res){
+  res.redirect('/');
+});
+app.get('/logout', facebookGetUser(), function(req, res){
+  req.user = null;
+  req.session.destroy();
+  res.redirect('/');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
