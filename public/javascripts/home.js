7c989ef59001e5f64f7c46a92c5d66fcdbf9c49f
Rachel Mathew <rachelmathew999@gmail.com>
2013-04-29 22:03:13 -0400

    Working navigation bar

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,14 +1,15 @@
 $(function(){
   $(document).ready(function() {
     console.log("ready");
-    $("#navbar_yourideas").hide();
 
-  //
-    $("#yourideas").click(function(event){
-  		$('#navbar_yourideas').show();
-  	});
-
-
-
+	$('#navbar_ideapanel').hide();	
+	$('#navbar_yourideas').click(function(){
+		console.log("You clicked it");
+		$('#navbar_ideapanel').slideDown('slow');
+	})
+	$('#navbar_yourideas_close').click(function(){
+		console.log("Closing your ideas");
+		$('#navbar_ideapanel').hide();
+	})
   	});
 })
